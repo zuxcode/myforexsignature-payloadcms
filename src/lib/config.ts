fbc40b8e8c70e81467c16f207f16a29ce4a7fae1
Alfred Nwanokwai <codeauthor1@gmail.com>
2026-01-27 19:18:58 +0100

    fix cor

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,10 +1,12 @@
-export const CSRF_WHITELIST_DOMAIN =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3001'
-    : // : 'https://myforexsignatureacademy.com'
-      'https://myforexsignature.vercel.app'
+export const CSRF_WHITELIST_DOMAIN = 'https://myforexsignature.vercel.app'
+// export const CSRF_WHITELIST_DOMAIN =
+//   process.env.NODE_ENV === 'development'
+//     ? 'http://localhost:3001'
+//     : // : 'https://myforexsignatureacademy.com'
+//       'https://myforexsignature.vercel.app'
 
-export const SERVER_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'https://myforexsignature-payloadcms.vercel.app/'
+export const SERVER_URL = 'https://myforexsignature-payloadcms.vercel.app/'
+// export const SERVER_URL =
+// process.env.NODE_ENV === 'development'
+//   ? 'http://localhost:3000'
+//   : 'https://myforexsignature-payloadcms.vercel.app/'
