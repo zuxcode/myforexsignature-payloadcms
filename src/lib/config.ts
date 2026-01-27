767933799843485ce715ec41523261f3c4446719
Alfred Nwanokwai <codeauthor1@gmail.com>
2026-01-27 21:36:13 +0100

    feat: cor

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,5 @@
 export const CSRF_WHITELIST_DOMAIN = 'https://myforexsignature.vercel.app'
+
 // export const CSRF_WHITELIST_DOMAIN =
 //   process.env.NODE_ENV === 'development'
 //     ? 'http://localhost:3001'
@@ -7,7 +8,4 @@ export const CSRF_WHITELIST_DOMAIN = 'https://myforexsignature.vercel.app'
 
 // export const SERVER_URL = 'http://localhost:3000'
 export const SERVER_URL = 'https://myforexsignature-payloadcms.vercel.app/'
-// export const SERVER_URL =
-// process.env.NODE_ENV === 'development'
-//   ? 'http://localhost:3000'
-//   : 'https://myforexsignature-payloadcms.vercel.app/'
+
